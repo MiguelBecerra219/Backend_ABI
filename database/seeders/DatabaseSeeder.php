75410b2bef546eed665b30b8512d890fb509cc2d
Miguel Becerra <mbecerra3@udi.edu.co>
2025-09-13 18:17:23 -0500

    Cambio de seeders y factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'state' => '1',
             'email' => 'test@example.com',
         ]);
     }
